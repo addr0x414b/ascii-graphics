01cfac66dac99d126eca1345865d3f5a83469411
addr0x414b <addr0x414b@proton.me>
2022-04-29 06:27:59 -0600

    Clean agm library code

--- agm.cpp
+++ agm.cpp
@@ -1,7 +1,7 @@
 #include "agm.hpp"
-#include <iostream>
 #include <math.h>
 
+/* Default constructor - Create 4x4 matrix with 0.0f */
 Mat4::Mat4() {
 	m.resize(4, std::vector<float>(4, 0.0f));
 }
@@ -14,8 +14,7 @@ Mat4::Mat4() {
  * @return Mat4: matrix with projection values */
 Mat4 perspective(float aspect, float fov, float zNear, float zFar) {
 	Mat4 m;
-
-	float fovRad = (fov/2.f) * (3.141592f / 180.f);
+	float fovRad = (fov/2.f) * (3.141592f / 180.f); // Convert to radians
 
 	m.m[0][0] = (1.f / (tan(fovRad))) / aspect;
 	m.m[1][1] = 1.f / tanf(fovRad);
@@ -23,64 +22,54 @@ Mat4 perspective(float aspect, float fov, float zNear, float zFar) {
 	m.m[3][2] = (-zNear * zFar) / (zFar - zNear);
 	m.m[2][3] = -1.0f;
 
-	/*
-	m.m[0][0] = 1.0f / (tanf((fov * (3.141592f / 180.f))/2.f)) / aspect;
-	m.m[1][1] = 1.0f / tanf((fov * (3.141592f / 180.f)/2.f));
-	m.m[2][2] = - ((zFar + zNear) / (zFar - zNear));
-	m.m[2][3] = - ((2 * zFar * zNear) / (zFar - zNear));
-	m.m[3][2] = -1.f;*/
-
 	return m;
 }
 
 /* Creates a Mat4 with the rotation matrix along the X axis
- * @param degrees the number of degrees to rotate
- * @return Mat4 rotation matrix */
+ * @param degrees: the number of degrees to rotate
+ * @return Mat4: rotation matrix */
 Mat4 rotX(float degrees) {
 	Mat4 m;
-
-	degrees = (degrees) * (3.141592f / 180.f);
+	float radians = (degrees) * (3.141592f / 180.f);
 
 	m.m[0][0] = 1.0f;
-	m.m[1][1] = cosf(degrees);
-	m.m[1][2] = -sinf(degrees);
-	m.m[2][1] = sinf(degrees);
-	m.m[2][2] = cosf(degrees);
+	m.m[1][1] = cosf(radians);
+	m.m[1][2] = -sinf(radians);
+	m.m[2][1] = sinf(radians);
+	m.m[2][2] = cosf(radians);
 	m.m[3][3] = 1.0f;
 
 	return m;
 }
 
 /* Creates a Mat4 with the rotation matrix along the Y axis
- * @param degrees the number of degrees to rotate
- * @return Mat4 rotation matrix */
+ * @param degrees: the number of degrees to rotate
+ * @return Mat4: rotation matrix */
 Mat4 rotY(float degrees) {
 	Mat4 m;
+	float radians = (degrees) * (3.141592f / 180.f);
 
-	degrees = (degrees) * (3.141592f / 180.f);
-
-	m.m[0][0] = cosf(degrees);
-	m.m[0][2] = sinf(degrees);
+	m.m[0][0] = cosf(radians);
+	m.m[0][2] = sinf(radians);
 	m.m[1][1] = 1.0f;
-	m.m[2][0] = -sinf(degrees);
-	m.m[2][2] = cosf(degrees);
+	m.m[2][0] = -sinf(radians);
+	m.m[2][2] = cosf(radians);
 	m.m[3][3] = 1.0f;
 
 	return m;
 }
 
 /* Creates a Mat4 with the rotation matrix along the Z axis
- * @param degrees the number of degrees to rotate
- * @return Mat4 rotation matrix */
+ * @param degrees: the number of degrees to rotate
+ * @return Mat4: rotation matrix */
 Mat4 rotZ(float degrees) {
 	Mat4 m;
+	float radians = (degrees) * (3.141592f / 180.f);
 
-	degrees = (degrees) * (3.141592f / 180.f);
-
-	m.m[0][0] = cosf(degrees);
-	m.m[0][1] = -sinf(degrees);
-	m.m[1][0] = sinf(degrees);
-	m.m[1][1] = cosf(degrees);
+	m.m[0][0] = cosf(radians);
+	m.m[0][1] = -sinf(radians);
+	m.m[1][0] = sinf(radians);
+	m.m[1][1] = cosf(radians);
 	m.m[2][2] = 1.f;
 	m.m[3][3] = 1.f;
 
@@ -90,11 +79,13 @@ Mat4 rotZ(float degrees) {
 /* Rotate the mesh
  * @params x,y,z amount in each axis */
 void Mesh::rotate(float x, float y, float z) {
-
 	Mat4 xMat = ::rotX(x);
 	Mat4 yMat = ::rotY(y);
 	Mat4 zMat = ::rotZ(z);
 
+	/* Since the mesh should rotate locally, it must untranslate back to the
+	 * origin, unrotate the mesh, then apply the new rotations, and then move the
+	 * mesh back to its original location */
 	unTranslate();
 	unRotateZ();
 	unRotateX();
@@ -124,7 +115,10 @@ void Mesh::rotate(float x, float y, float z) {
 		trig.norms[2] = mult4(trig.norms[2], zMat);
 		trig.fNormal = mult4(trig.fNormal, zMat);
 	}
-	staticTranslate(transAmt.x, transAmt.y, transAmt.z);
+	staticTranslate(transAmt.x, transAmt.y, transAmt.z); // Bring the mesh back
+
+	/* These amounts are used to unrotate the mesh. Set them here and then
+	 * use them next time the unrotate functions are called */
 	rotAmt.x = x;
 	rotAmt.y = y;
 	rotAmt.z = z;
@@ -134,9 +128,12 @@ void Mesh::rotate(float x, float y, float z) {
 /* Translate the mesh
  * @params x,y,z amount to translate in the x,y,z axis */
 void Mesh::translate(float x, float y, float z) {
+	/* Set the translated amounts. These are used when the mesh is untranslated
+	 * for its local rotations */
 	transAmt.x += x;
 	transAmt.y += y;
 	transAmt.z += z;
+
 	for (auto &trig : trigs) {
 		trig.verts[0].x += x;
 		trig.verts[0].y += y;
@@ -155,9 +152,6 @@ void Mesh::translate(float x, float y, float z) {
 /* Translate the mesh statically, as in set the points and that's it
  * @params x,y,z amount to translate in the x,y,z axis */
 void Mesh::staticTranslate(float x, float y, float z) {
-	//transAmt.x = x;
-	//transAmt.y = y;
-	//transAmt.z = z;
 	for (auto &trig : trigs) {
 		trig.verts[0].x += x;
 		trig.verts[0].y += y;
@@ -273,15 +267,12 @@ void Mesh::unTranslate() {
 		trig.verts[2].z -= transAmt.z;
 
 	}
-	//transAmt.x = 0.0f;
-	//transAmt.y = 0.0f;
-	//transAmt.z = 0.0f;
 }
 
-/* Multiply a vertex by a 4x4 matrix
- * @param v our vertex
- * @param m our 4x4 matrix
- * @return product of multiplcation */
+/* Multiply a vertex by a 4x4 matrix - performs division by w
+ * @param v: the vertex
+ * @param m: the 4x4 matrix
+ * @return Vert: product of multiplcation */
 Vert mult4(Vert v, Mat4& m) {
 
 	Vert a;
@@ -310,31 +301,31 @@ Vert mult4(Vert v, Mat4& m) {
 }
 
 /* Calculate the dot product between two vertices
- * @params a,b vertices
- * @return float result */
+ * @params a,b: vertices
+ * @return float: result */
 float dot(Vert a, Vert b) {
 	return ((a.x*b.x) + (a.y*b.y) + (a.z*b.z));
 }
 
-/* Calculate the direction vector b->a
- * @params a,b vertices
- * @return Vert direction vector result */
+/* Calculate the direction vector a-b
+ * @params a,b: vertices
+ * @return Vert: direction vector result */
 Vert direc(Vert a, Vert b) {
 	Vert r(a.x-b.x, a.y-b.y, a.z-b.z);
 	return r;
 }
 
 /* Calculate the cross product of two vectors
- * @params a,b vertices
- * @return Vert cross product result */
+ * @params a,b: vertices
+ * @return Vert: cross product result */
 Vert cross(Vert a, Vert b) {
 	Vert c((a.y*b.z)-(a.z*b.y), (a.z*b.x)-(a.x*b.z), (a.x*b.y)-(a.y*b.x));
 	return c;
 }
 
 /* Project a triangle with a perspective matrix
- * @param t the triangle
- * @param m the perspective matrix */
+ * @param t: the triangle
+ * @param m: the perspective matrix */
 void project(Trig& t, Mat4 m) {
 	t.verts[0] = mult4(t.verts[0], m);
 	t.verts[1] = mult4(t.verts[1], m);
@@ -342,21 +333,21 @@ void project(Trig& t, Mat4 m) {
 }
 
 /* Calculate the Z value from x,y using cross and vert of equation of plane
- * @params x,y our input x and y values
- * @param c cross product vertex from equation of plane
- * @param v point from our original triangle from equation of plane */
+ * @params x,y: the input x and y values
+ * @param c: cross product vertex from equation of plane
+ * @param v: point from our original triangle from equation of plane
+ * @return float: the z value */
 float calcZ(float x, float y, Vert c, Vert v) {
 	float k = dot(c, v);
 	float z = ((c.x*x + c.y*y - k) / (-c.z));
-	//float z = (1.f/c.z)*(c.x*v.x + c.y*v.y + c.z*v.z - c.x*x - c.y*y);
 	return z;
 }
 
-/* Calculate the bary coordinates given 3 points of a triangle and and x and y
+/* Calculate the bary coordinates given 3 points of a triangle and x and y
  * inside of the triangle
- * @params p1,p2,p3 vertices of the triangle
- * @params x,y current point within the triangle
- * @params w1,w2,w3 store the bary outputs */
+ * @params p1,p2,p3: vertices of the triangle
+ * @params x,y: current point within the triangle
+ * @params w1,w2,w3: store the bary outputs */
 void calcBary(Vert p1, Vert p2, Vert p3, int x, int y, float& w1, float& w2,
 		float& w3) {
 	w1 = (((p2.y - p3.y) * (x - p3.x)) + (p3.x - p2.x) * (y - p3.y)) /
--- agm.hpp
+++ agm.hpp
@@ -29,52 +29,53 @@ Mat4 perspective(float aspect, float fov, float zNear, float zFar);
 Mat4 rotZ(float degrees);
 
 /* Creates a Mat4 with the rotation matrix along the Y axis
- * @param degrees the number of degrees to rotate
- * @return Mat4 rotation matrix */
+ * @param degrees: the number of degrees to rotate
+ * @return Mat4: rotation matrix */
 Mat4 rotY(float degrees);
 
 /* Creates a Mat4 with the rotation matrix along the X axis
- * @param degrees the number of degrees to rotate
- * @return Mat4 rotation matrix */
+ * @param degrees: the number of degrees to rotate
+ * @return Mat4: rotation matrix */
 Mat4 rotX(float degrees);
 
-/* Multiply a vertex by a 4x4 matrix
- * @param v our vertex
- * @param m our 4x4 matrix
- * @return product of multiplcation */
+/* Multiply a vertex by a 4x4 matrix - performs division by w
+ * @param v: the vertex
+ * @param m: the 4x4 matrix
+ * @return Vert: product of multiplcation */
 Vert mult4(Vert v, Mat4& m);
 
 /* Calculate the dot product between two vertices
- * @params a,b vertices
- * @return float result */
+ * @params a,b: vertices
+ * @return float: result */
 float dot(Vert a, Vert b);
 
-/* Calculate the direction vector b->a
- * @params a,b vertices
- * @return Vert direction vector result */
+/* Calculate the direction vector a-b
+ * @params a,b: vertices
+ * @return Vert: direction vector result */
 Vert direc(Vert a, Vert b);
 
 /* Calculate the cross product of two vectors
- * @params a,b vertices
- * @return Vert cross product result */
+ * @params a,b: vertices
+ * @return Vert: cross product result */
 Vert cross(Vert a, Vert b);
 
 /* Calculate the Z value from x,y using cross and vert of equation of plane
- * @params x,y our input x and y values
- * @param c cross product vertex from equation of plane
- * @param v point from our original triangle from equation of plane */
+ * @params x,y: the input x and y values
+ * @param c: cross product vertex from equation of plane
+ * @param v: point from our original triangle from equation of plane
+ * @return float: the z value */
 float calcZ(float x, float y, Vert c, Vert v);
 
 /* Project a triangle with a perspective matrix
- * @param t the triangle
- * @param m the perspective matrix */
+ * @param t: the triangle
+ * @param m: the perspective matrix */
 void project(Trig& t, Mat4 m);
 
-/* Calculate the bary coordinates given 3 points of a triangle and and x and y
+/* Calculate the bary coordinates given 3 points of a triangle and x and y
  * inside of the triangle
- * @params p1,p2,p3 vertices of the triangle
- * @params x,y current point within the triangle
- * @params w1,w2,w3 store the bary outputs */
+ * @params p1,p2,p3: vertices of the triangle
+ * @params x,y: current point within the triangle
+ * @params w1,w2,w3: store the bary outputs */
 void calcBary(Vert p1, Vert p2, Vert p3, int x, int y, float& w1, float& w2,
 		float& w3);
 
